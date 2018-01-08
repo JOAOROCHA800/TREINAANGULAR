39c70ea3c261d6bbf5e2a906ef1be5621860ddc7
Joao Carlos <joao.rocha800@gmail.com>
2018-01-08 11:04:40 -0200

    primeiro commit

---
+++ src/app/order/order.component.ts
@@ -0,0 +1,22 @@
+import { Component, OnInit } from '@angular/core';
+import {RadioOption} from '../shared/radio/radio-option.model'
+
+@Component({
+  selector: 'mt-order',
+  templateUrl: './order.component.html',
+  styleUrls: ['./order.component.css']
+})
+export class OrderComponent implements OnInit {
+
+  paymentOption: RadioOption[] = [
+    {label: 'Dinheiro', value: 'MON'},
+    {label: 'Cartão de Débito', value: 'DEB'},
+    {label: 'Cartão Refeição', value: 'REF'}
+  ]
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
