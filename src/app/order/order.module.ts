8951d3b379b73f2becaf65906f3d0d9c814cf488
Joao Carlos <joao.rocha800@gmail.com>
2018-01-25 20:49:03 -0200

    Conclusão aula 85

--- src/app/order/order.module.ts
+++ src/app/order/order.module.ts
@@ -1,6 +1,6 @@
 import {NgModule} from '@angular/core'
 import {RouterModule, Routes} from "@angular/router"
-import {SharedModule} from '../Shared/shared.module'
+import {SharedModule} from '../shared/shared.module'
 import {OrderComponent} from './order.component'
 import {OrderItemsComponent} from './order-items/order-items.component'
 import {DeliveryCostsComponent} from './delivery-costs/delivery-costs.component'
